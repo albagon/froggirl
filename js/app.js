612cd0c255bf9ed390b54a7c19d9d61dfa5a8ef1
albagonzalez <albupas@hotmail.com>
2018-04-13 22:51:26 +0100

    feat: Add properties to the Enemy constructor

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+// Parameter: y, can be 1, 2 or 3 because those are the stone rows
+var Enemy = function(y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // This column value should start in 1 because the enemy always
+    // moves from the beginning of the canvas from left to right
+    this.x = 1;
+    //this row value should be any of the 3 stone rows
+    this.y = y * 83;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x * dt; //I added this line
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +28,15 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// These are the cell sizes in the canvas: col * 101, row * 83
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
+let enemy1 = new Enemy(1);
+allEnemies.push(enemy1);
 
 
 // This listens for key presses and sends the keys to your
