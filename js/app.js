e447e81b815498dfae1b98034dcb4498487df4c5
albagonzalez <albupas@hotmail.com>
2018-04-14 00:21:54 +0100

    fix: Make the enemy update/move correctly

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x * dt;
+    this.x = this.x + (dt * 150);
 };
 
 // Draw the enemy on the screen, required method for game
